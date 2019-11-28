59a6c6b6c340d97292a92fc044a6555ded6c8699
jansimrp <57145682+jansimrp@users.noreply.github.com>
2019-11-28 18:03:48 +0530

    commiting payments,analytis and notifications app

---
+++ Code/bin_manager/mgr_Analytics/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MgrAnalyticsConfig(AppConfig):
+    name = 'mgr_Analytics'
---
+++ Code/bin_manager/mgr_notifications/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MgrNotificationsConfig(AppConfig):
+    name = 'mgr_notifications'
